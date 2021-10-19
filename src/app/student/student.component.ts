7a3e72f3faf7f5efd1951f2f57545a1736db90ba
Aline4018 <aline12de@gmail.com>
2021-10-19 09:38:09 -0300

    Criação do Import, evolução de alguns atributos.

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,19 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import{Student} from './student.model'
 @Component({
   selector: 'jad-student',
   templateUrl: './student.component.html',
   styleUrls: ['./student.component.css']
 })
 export class StudentComponent implements OnInit {
+@Input() student: Student
+  constructor( ) { }
 
-  name: string = 'Luke'
-  isJedi: boolean = true
-
-
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
