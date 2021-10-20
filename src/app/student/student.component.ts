df829176f8bb31d4a02bd3ff70a7d60fa803d181
Aline4018 <aline12de@gmail.com>
2021-10-20 11:08:15 -0300

    Solução do Input

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit, Input } from '@angular/core';
-import{Student} from './student.model'
+import {Student} from './student.model'
 @Component({
   selector: 'jad-student',
   templateUrl: './student.component.html',
   styleUrls: ['./student.component.css']
 })
 export class StudentComponent implements OnInit {
-@Input() student: Student
+  @Input() student: Student
+
   constructor( ) { }
 
   ngOnInit() {
--- src/app/student/student.model.ts
+++ src/app/student/student.model.ts
@@ -1,4 +1,4 @@
-export interface student {
+export interface Student {
   name: string
   isJedi: boolean
   temple?: string
